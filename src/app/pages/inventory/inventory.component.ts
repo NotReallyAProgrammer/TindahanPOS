b1894951eb8b4e17511a36291ff2f866e4fefc6f
NotReallyAProgrammer <73622276+NotReallyAProgrammer@users.noreply.github.com>
2024-04-05 21:09:56 +0800

    .

--- src/app/pages/inventory/inventory.component.ts
+++ src/app/pages/inventory/inventory.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { DashboardService } from '../../services/dashboard.service';
 
 @Component({
@@ -13,6 +13,13 @@ export class InventoryComponent {
 
   isAddMenuOpen: boolean = false;
   $data!: Observable<Array<any>>;
+  $catData!: Observable<Array<any>>;
+
+  itemCount!: number;
+  totalVal!: number;
+  totalSelling!: number;
+
+  catCount!: number;
 
   ngOnInit() {
     this.loadItems();
@@ -20,6 +27,23 @@ export class InventoryComponent {
 
   loadItems(): void {
     this.$data = this.dashService.loadItems();
+    this.$catData = this.dashService.loadCategories();
+
+    this.$catData.subscribe((data) => {
+      this.catCount = data.length;
+    });
+
+    this.$data.subscribe((data) => {
+      this.itemCount = data.length;
+
+      this.totalVal = data
+        .flat()
+        .reduce((sum, el) => (sum += el.itemSelling * el.itemQty), 0);
+
+      this.totalSelling = data
+        .flat()
+        .reduce((sum, el) => (sum += el.itemPrice * el.itemQty), 0);
+    });
   }
 
   openMenu(): void {
